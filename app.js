7dbe4846d29658e9d2fd27e721fe2b01349638c9
Preston <pbriggs28@users.noreply.github.com>
2017-11-15 15:01:56 -0800

    Lesson 2.1: The IF condition

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ var robotTwoScore = 500;
 
 console.log(robotOneName + " score: " + robotOneScore);
 console.log(robotTwoName + " score: " + robotTwoScore);
+
+if(robotOneScore > robotTwoScore) {
+	console.log("Robot one won!");
+} else if(robotTwoScore > robotOneScore) {
+	console.log("Robot two won!");
+} else if(robotOneScore == robotTwoScore) {
+	console.log("It was a tie!");
+}
